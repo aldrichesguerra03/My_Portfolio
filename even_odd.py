59f539c125dfb3920028d22ff95c8ae7b0d7ec78
Aldrich <aldrichaboesguerra@gmail.com>
2022-08-29 12:50:36 +0800

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,8 +1,4 @@
-# 🚨 Don't change the code below 👇
 number = int(input("Which number do you want to check? "))
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 
 input_number = number % 2
 
