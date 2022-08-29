37aa54a653fbd35d2cd50b51c69f13312d8c6215
Aldrich <aldrichaboesguerra@gmail.com>
2022-08-29 12:52:33 +0800

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -27,7 +27,6 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
 game_images = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 print(game_images[user_choice])
