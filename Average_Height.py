a7efadad182ebdbab747e98b5c67e1fbbe714e6b
Aldrich <aldrichaboesguerra@gmail.com>
2022-08-29 12:56:33 +0800

    Update Average_Height.py

--- Average_Height.py
+++ Average_Height.py
@@ -1,11 +1,7 @@
-# 🚨 Don't change the code below 👇
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
-
-
-#Write your code below this row 👇
+  
 total_height = 0
 student_heights_length = len(student_heights)
 for height in student_heights:
