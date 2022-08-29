13d5a8642561fab9d5849731c280d8ca227db3d4
Aldrich <aldrichaboesguerra@gmail.com>
2022-08-29 12:55:50 +0800

    Update BMI_Calculator.py

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,9 +1,5 @@
-# 🚨 Don't change the code below 👇
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 
 new_height = float(height)
 new_weight = int(weight)
